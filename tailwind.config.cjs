dfcb73f5c021343f5f17b0ce861f0ede3e937ff2
Lovell Liu <lovelliu@foxmail.com>
2023-01-02 17:04:17 +0800

    chore: typo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				day: 'hsl(210, 29%, 97%)',
-				dark: 'hsl(217, 24%, 17%)'
-			}
-		},
-	},
-	darkMode: 'class',
-	plugins: [
-		require('@tailwindcss/typography')
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        day: 'hsl(210, 29%, 97%)',
+        dark: 'hsl(217, 24%, 17%)',
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
