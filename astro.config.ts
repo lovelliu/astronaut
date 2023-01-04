85261b31168c390803c7c6ddc4a62119f6ec2692
Lovell Liu <lovelliu@foxmail.com>
2023-01-04 21:58:07 +0800

    feat: add sitemap config

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 import { setThemeIntegration } from './src/utils/setThemeIntegration'
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
     setThemeIntegration(),
+    sitemap(),
   ],
+  site: 'https://lovelliu.me',
 })
