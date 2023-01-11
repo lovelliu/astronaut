f0a261280cf39dd0906e87afaf20429bbeb7976a
Lovell Liu <lovelliu@foxmail.com>
2023-01-11 20:56:20 +0800

    feat: add rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss'
+
+const postImportResult = import.meta.glob('../posts/**/*.md', { eager: true })
+const posts = Object.values(postImportResult)
+
+export const get = () => rss({
+  title: 'Lovell Liu',
+  description: 'Lovell Liu‘s blog',
+  site: import.meta.env.SITE,
+  items: posts.map((post: any) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.date,
+  })),
+  customData: '<language>zh-cn</language>',
+  stylesheet: '/styles.xsl',
+})
